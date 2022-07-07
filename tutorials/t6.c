3e902e1b3bb12fac89642652ff15e559f397f8cf
mtbthebest <mtbthebest11@gmail.com>
2022-07-07 22:19:51 +0900

    control statement

--- tutorials/t6.c
+++ tutorials/t6.c
@@ -4,6 +4,7 @@
 
 #include "stdio.h"
 #include "ctype.h"
+#include "stdlib.h"
 #include "iso646.h"
 
 
@@ -32,6 +33,62 @@ void fn2(){
 
 }
 
+typedef int (*cmp)(void*, void*) ;
+void sort(void *array, long size, size_t el_size, cmp fn){
+    void *a, *b;
+    int result;
+    int tmp;
+    for(int j=0; j < size; j++){
+        for(int k=j+1; k<size;k++){
+            a = array + j * el_size;
+            b = array + k * el_size;
+            result = fn(a, b);
+            if(result == -1)
+            {
+                tmp = *(int *)b;
+                *(int *)b= *(int *)a;
+                *(int *)a= tmp;
+            }
+
+        }
+    }
+}
+
+int cmp_fn(void *a, void *b){
+    int c = *((int *)a);
+    int d = *((int *)b);
+    return d > c ? 1 : (d < c ? -1: 0 );
+
+}
+
+void fn3(){
+    const int ARR_SIZE = 50;
+    int *array = malloc(ARR_SIZE * sizeof(int));
+    if(array != NULL){
+        for(int i=0;i<ARR_SIZE; i++){
+            array[i] =  (lrand48()  * 10) / RAND_MAX;
+        }
+        sort(array, ARR_SIZE, sizeof(int), cmp_fn);
+        for(int i=0; i < ARR_SIZE; i++){
+            printf("%d \n", array[i]);
+        }
+    }
+
+    free(array);
+}
+
+void fn4(){
+
+    char ch;
+
+    while((ch = getchar()) != 'q'){
+        if(ch == '\n' or ch=='d')
+            continue;
+
+        printf("ch: %c \n", ch);
+    }
+}
+
 int main(){
-    fn2();
+    fn4();
 }
